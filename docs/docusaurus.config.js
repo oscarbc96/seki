973c534b3c10d3815db2628b56aff84eaef56d28
Oscar Blanco Castan <oscarbc1996@gmail.com>
2022-09-04 02:41:11 +0200

    fix

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,10 +6,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Seki',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://oscarbc96.github.io',
+  baseUrl: '/seki/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
